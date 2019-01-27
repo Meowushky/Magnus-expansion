f03f27302638557522f61b3533f40eee3ef62f55
arina <arinolololo@mail.ru>
2019-01-28 02:15:15 +0800

    Считывает с файла теперь также b,E,tol,s12,s13,model,out

--- M4/lua.c
+++ M4/lua.c
@@ -14,7 +14,8 @@ enum
 int main(int argc, char **argv)
 {
   int m_len,stat;
-  double a=0.1;
+  double a=0.1,b,E,tol,s12,s13;
+  const char *out,*model;
   char tmp[MAX_READ_LEN];
   //char filen[MAX_LEN];
 
@@ -95,7 +96,98 @@ int main(int argc, char **argv)
     a=lua_tonumber(L,-1);
     lua_pop(L,1);
   }
-  fprintf(stderr,"a=%lf",a);
+  fprintf(stderr,"a=%lf\n",a);
+  
+  stat=lua_getglobal(L,"b");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","b");
+  }
+  
+  if(stat==LUA_TNUMBER)
+  {
+    b=lua_tonumber(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"b=%lf\n",b);
+  
+  stat=lua_getglobal(L,"E");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","E");
+  }
+  
+  if(stat==LUA_TNUMBER)
+  {
+    E=lua_tonumber(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"E=%lf\n",E);
+  
+  stat=lua_getglobal(L,"tol");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","tol");
+  }
+  
+  if(stat==LUA_TNUMBER)
+  {
+    tol=lua_tonumber(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"tol=%lf\n",tol);
+  
+  stat=lua_getglobal(L,"s12");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","s12");
+  }
+  
+  if(stat==LUA_TNUMBER)
+  {
+    s12=lua_tonumber(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"s12=%lf\n",s12);
+  
+    stat=lua_getglobal(L,"s13");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","s13");
+  }
+  
+  if(stat==LUA_TNUMBER)
+  {
+    s13=lua_tonumber(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"s13=%lf\n",s13);
+  
+    stat=lua_getglobal(L,"out");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","out");
+  }
+  
+  if(stat==LUA_TSTRING)
+  {
+    out=lua_tostring(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"out=%s\n",out);
+  
+    stat=lua_getglobal(L,"model");
+  if(stat==LUA_TNONE||stat==LUA_TNIL)
+  {
+    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","model");
+  }
+  
+  if(stat==LUA_TSTRING)
+  {
+    model=lua_tostring(L,-1);
+    lua_pop(L,1);
+  }
+  fprintf(stderr,"model=%s\n",model);
   
   lua_close(L);
 
