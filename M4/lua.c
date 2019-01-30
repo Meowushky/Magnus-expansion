1cf8862c254f5af3582e17b75a0bb118eb0b930c
arina <arinolololo@mail.ru>
2019-01-30 14:23:57 +0800

    До внедрения lua в M4

--- M4/lua.c
+++ M4/lua.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <complex.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <lua.h>
@@ -9,15 +11,62 @@ enum
 {
   MAX_LEN=256,
   MAX_READ_LEN=1024,
+  FLAVS=3
 };
+
+enum
+{
+  P_A=0,
+  P_B,
+  P_E,
+  P_TOL,
+  P_S12,
+  P_S13,
+  P_S23,
+  P_OUT,
+  P_MODEL,
+  P_PSI0,
+  P_EOPL,
+  PARAMS=10
+};
+
+enum
+{
+  CPD_NUMBER=0,
+  CPD_CX_NUMBER,
+  CPD_STRING,
+  CPD_VECTOR,
+  CPD_CX_VECTOR,
+  CPD_MATRIX,
+  CPD_CX_MATRIX
+};
+
+typedef union
+{
+  double v; //value
+  char n[MAX_LEN]; //name
+  double complex z[FLAVS]; //psi0
+} conf_par;
+
+typedef struct
+{
+  uint8_t tag;
+  conf_par par;
+  char name[MAX_LEN];
+} conf_data;
+
+void init_conf(conf_data*);
+void print_conf(FILE*, conf_data*);
 
 int main(int argc, char **argv)
 {
   int m_len,stat;
-  double a=0.1,b,E,tol,s12,s13;
-  const char *out,*model;
+  double Re,Im;
   char tmp[MAX_READ_LEN];
-  //char filen[MAX_LEN];
+  conf_data cfg[PARAMS];
+  init_conf(cfg);
+  
+  FILE *stream=fopen("params.dat","w");
 
   if(argc==1)
   {
@@ -57,7 +106,6 @@ int main(int argc, char **argv)
     {
      fprintf(stderr,"Такого файла нет: %s.",tmp);
      return 1;
-     
     }
     else
     {
@@ -85,111 +133,121 @@ int main(int argc, char **argv)
     lua_close(L);
     return 2;
   }
-  stat=lua_getglobal(L,"a");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","a");
-  }
   
-  if(stat==LUA_TNUMBER)
+  for(int j1=0;j1<PARAMS;j1++)
   {
-    a=lua_tonumber(L,-1);
-    lua_pop(L,1);
+    stat=lua_getglobal(L,cfg[j1].name);
+    if(stat==LUA_TNONE||stat==LUA_TNIL)
+    {
+      fprintf(stderr,"Такого параметра нет в файле: (%s).\n",cfg[j1].name);
+      continue;
+    }
+    switch(stat)
+    {
+      case LUA_TNUMBER:
+      cfg[j1].par.v=lua_tonumber(L,-1);
+      lua_pop(L,1);
+      break;
+      
+      case LUA_TSTRING:
+      strncpy(cfg[j1].par.n,lua_tostring(L,-1),MAX_LEN);
+      lua_pop(L,1);
+      break;
+      
+      case LUA_TTABLE:
+      for(int i=1;i-1<FLAVS;i++)
+      {
+        lua_pushnumber(L,i);
+        stat=lua_gettable(L,-2);
+    
+        if(LUA_TTABLE!=stat)
+        {
+          fprintf(stderr,"Значение для %s в файле имеет неверный формат.\n",cfg[j1].name);
+          return 1;
+        }
+    
+        lua_pushnumber(L,1);
+        stat=lua_gettable(L,-2);
+        Re=lua_tonumber(L,-1);
+        lua_pop(L,1);
+    
+        lua_pushnumber(L,2);
+        stat=lua_gettable(L,-2);
+        Im=lua_tonumber(L,-1);
+        lua_pop(L,2);
+    
+        cfg[j1].par.z[i-1]=Re+I*Im;
+      }
+    }
   }
-  fprintf(stderr,"a=%lf\n",a);
-  
-  stat=lua_getglobal(L,"b");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","b");
-  }
-  
-  if(stat==LUA_TNUMBER)
-  {
-    b=lua_tonumber(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"b=%lf\n",b);
-  
-  stat=lua_getglobal(L,"E");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","E");
-  }
-  
-  if(stat==LUA_TNUMBER)
-  {
-    E=lua_tonumber(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"E=%lf\n",E);
-  
-  stat=lua_getglobal(L,"tol");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","tol");
-  }
-  
-  if(stat==LUA_TNUMBER)
-  {
-    tol=lua_tonumber(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"tol=%lf\n",tol);
-  
-  stat=lua_getglobal(L,"s12");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","s12");
-  }
-  
-  if(stat==LUA_TNUMBER)
-  {
-    s12=lua_tonumber(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"s12=%lf\n",s12);
-  
-    stat=lua_getglobal(L,"s13");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","s13");
-  }
-  
-  if(stat==LUA_TNUMBER)
-  {
-    s13=lua_tonumber(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"s13=%lf\n",s13);
-  
-    stat=lua_getglobal(L,"out");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","out");
-  }
-  
-  if(stat==LUA_TSTRING)
-  {
-    out=lua_tostring(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"out=%s\n",out);
-  
-    stat=lua_getglobal(L,"model");
-  if(stat==LUA_TNONE||stat==LUA_TNIL)
-  {
-    fprintf(stderr,"Такого параметра нет в файле: (%s).\n","model");
-  }
-  
-  if(stat==LUA_TSTRING)
-  {
-    model=lua_tostring(L,-1);
-    lua_pop(L,1);
-  }
-  fprintf(stderr,"model=%s\n",model);
-  
-  lua_close(L);
 
+  print_conf(stream,cfg);
+
+  lua_close(L);
+  fclose(stream);
+  
   return 0;  
+}
+
+void init_conf(conf_data *cfg)
+{
+  cfg[P_A].par.v=0.1;
+  strncpy(cfg[P_A].name,"a",MAX_LEN);
+  cfg[P_A].tag=CPD_NUMBER;
+  
+  cfg[P_B].par.v=1.0;
+  strncpy(cfg[P_B].name,"b",MAX_LEN);
+  cfg[P_B].tag=CPD_NUMBER;
+  
+  cfg[P_E].par.v=1.0;
+  strncpy(cfg[P_E].name,"E",MAX_LEN);
+  cfg[P_E].tag=CPD_NUMBER;
+  
+  cfg[P_TOL].par.v=1e-4;
+  strncpy(cfg[P_TOL].name,"tol",MAX_LEN);
+  cfg[P_TOL].tag=CPD_NUMBER;
+  
+  cfg[P_S12].par.v=sqrt(0.308);
+  strncpy(cfg[P_S12].name,"s12",MAX_LEN);
+  cfg[P_S12].tag=CPD_NUMBER;
+  
+  cfg[P_S13].par.v=sqrt(0.0234);
+  strncpy(cfg[P_S13].name,"s13",MAX_LEN);
+  cfg[P_S13].tag=CPD_NUMBER;
+  
+  cfg[P_S23].par.v=sqrt(0.437);
+  strncpy(cfg[P_S23].name,"s23",MAX_LEN);
+  cfg[P_S23].tag=CPD_NUMBER;
+  
+  strncpy(cfg[P_OUT].par.n,"screen",MAX_LEN);
+  strncpy(cfg[P_OUT].name,"out",MAX_LEN);
+  cfg[P_OUT].tag=CPD_STRING;
+  
+  strncpy(cfg[P_MODEL].par.n,"sun",MAX_LEN);
+  strncpy(cfg[P_MODEL].name,"model",MAX_LEN);
+  cfg[P_MODEL].tag=CPD_STRING;
+  
+  cfg[P_PSI0].par.z[0]=-1;
+  cfg[P_PSI0].par.z[1]=-1;
+  cfg[P_PSI0].par.z[2]=-1;
+  strncpy(cfg[P_PSI0].name,"psi0",MAX_LEN);
+  cfg[P_PSI0].tag=CPD_CX_NUMBER;
+}
+
+void print_conf(FILE *stream, conf_data *cfg)
+{  
+  fprintf(stream,"%s=%lf\n",cfg[P_A].name,cfg[P_A].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_B].name,cfg[P_B].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_E].name,cfg[P_E].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_TOL].name,cfg[P_TOL].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_S12].name,cfg[P_S12].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_S13].name,cfg[P_S13].par.v);
+  fprintf(stream,"%s=%lf\n",cfg[P_S23].name,cfg[P_S23].par.v);
+  fprintf(stream,"%s=%s\n",cfg[P_OUT].name,cfg[P_OUT].par.n);
+  fprintf(stream,"%s=%s\n",cfg[P_MODEL].name,cfg[P_MODEL].par.n);
+  
+  fprintf(stream,"%s=\n",cfg[P_PSI0].name);
+  fprintf(stream,"(%lf,%lf)\n",creal(cfg[P_PSI0].par.z[0]),cimag(cfg[P_PSI0].par.z[0]));
+  fprintf(stream,"(%lf,%lf)\n",creal(cfg[P_PSI0].par.z[1]),cimag(cfg[P_PSI0].par.z[1]));
+  fprintf(stream,"(%lf,%lf)\n",creal(cfg[P_PSI0].par.z[2]),cimag(cfg[P_PSI0].par.z[2]));
 }
