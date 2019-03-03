5a3c482e0b5efe82fe4f51ef87ff2e7f09dc8b50
arina <arinolololo@mail.ru>
2019-03-03 20:10:53 +0800

    Увеличение значения psi0_norm_accuracy в 30 раз

--- M4/M4.c
+++ M4/M4.c
@@ -245,7 +245,7 @@ int main(int argc,char **argv)
   mod2+=cfg[P_PSI0].par.z[1]*conj(cfg[P_PSI0].par.z[1]);
   mod2+=cfg[P_PSI0].par.z[2]*conj(cfg[P_PSI0].par.z[2]);
   
-  cfg[P_PSI0_NORM_ACC].par.v=1.2*cfg[P_PSI0_NORM_ACC].par.v; 
+  cfg[P_PSI0_NORM_ACC].par.v=30.*cfg[P_PSI0_NORM_ACC].par.v; 
   
   if(fabs(creal(mod2)-1.)>fabs(cfg[P_PSI0_NORM_ACC].par.v))
   {
@@ -361,14 +361,14 @@ int main(int argc,char **argv)
   fprintf(stream,"# prev. step=%4.3e\n", res.prev_step);
   fprintf(stream,"# last step=%4.3e\n", res.last_step);
   fprintf(stream,"# Er=%4.3e\n",res.Er);
-  fprintf(stream,"# psi={{%12.11lf,%12.11lf},{%12.11lf,%12.11lf},{%12.11lf,%12.11lf}}\n",
+  fprintf(stream,"# psi={{%17.16lf,%17.16lf},{%17.16lf,%17.16lf},{%17.16lf,%17.16lf}}\n",
     creal(res.Psi[0]),cimag(res.Psi[0]),
     creal(res.Psi[1]),cimag(res.Psi[1]),
     creal(res.Psi[2]),cimag(res.Psi[2]));
-  fprintf(stream,"# ||psi|^2-1|=%4.5e\n", fabs(creal(mod2)-1.));
+  fprintf(stream,"# ||psi|^2-1|=%4.3e\n", fabs(creal(mod2)-1.));
   
   fprintf(stream,"# a b E Pee\n");  
-  fprintf(stream,"%lf\t%lf\t%lf\t%12.11lf\n", d0, d1, E, Pee);
+  fprintf(stream,"%lf\t%lf\t%17.16lf\t%17.16lf\n", d0, d1, E, Pee);
   
   if(stream!=stderr)
   {
